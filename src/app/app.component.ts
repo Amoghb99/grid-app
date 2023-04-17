8d07c86185d6fcc10d408306418dc8eda92672d9
Amogh Bagkar <amoghhh123@gmail.com>
2023-04-18 01:45:59 +0530

    grid-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'grid-app';
+
+  cells: any = new Array(64).fill(0);
+  selectedCell: number | null = null;
+
+  onCellClick(index: number): void {
+    this.selectedCell = index;
+  }
+
+  isBlackCell(index: number): boolean {
+    const row = Math.floor(index / 8);
+    const col = index % 8;
+    return (row + col) % 2 === 0;
+  }
+
+  isWhiteCell(index: number): boolean {
+    return !this.isBlackCell(index);
+  }
+
 }
